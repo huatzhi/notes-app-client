3384626a0e1616d12a9ccba2c60dc9c064143649
huat <huat@snsoft.my>
2017-07-27 12:11:07 +0800

    Load the State from the Session

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
 import RouteNavItem from './components/RouteNavItem';
 import './App.css';
 import Routes from './Routes';
+import { CognitoUserPool, } from 'amazon-cognito-identity-js';
+import config from './config.js';
 
 // todo::it will probably be better that navbar should have one component of its
 // own
@@ -20,7 +22,28 @@ class App extends Component {
 
     this.state = {
       userToken: null,
+      isLoadingUserToken: true
     };
+  }
+
+  async componentDidMount() {
+    const currentUser = this.getCurrentUser();
+
+    if (currentUser === null) {
+      this.setState({isLoadingUserToken: false});
+      return;
+    }
+
+    try {
+      const userToken = await this.getUserToken(currentUser);
+      this.updateUserToken(userToken);
+    }
+    catch(e) {
+      console.error(e);
+      alert(e);
+    }
+
+    this.setState({isLoadingUserToken: false});
   }
 
   updateUserToken = (userToken) => {
@@ -38,13 +61,35 @@ class App extends Component {
     this.updateUserToken(null);
   }
 
+  getCurrentUser() {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    return userPool.getCurrentUser();
+  }
+
+  getUserToken(currentUser) {
+    return new Promise((resolve, reject) => {
+      currentUser.getSession(function(err, session) {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(session.getIdToken().getJwtToken());
+      });
+    });
+  }
+
   render() {
     const childProps = {
       userToken: this.state.userToken,
       updateUserToken: this.updateUserToken,
     };
 
-    return (
+    return ! this.state.isLoadingUserToken
+    &&
+    (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
