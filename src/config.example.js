fae7665deb4f282934c24b97a649b7391274ff92
huat <huat@snsoft.my>
2017-07-27 11:23:34 +0800

    cognito login

---
+++ src/config.example.js
@@ -0,0 +1,9 @@
+// removet the '.example' from the file path
+// replace the pool id and client id according to your app
+
+export default {
+  cognito: {
+    USER_POOL_ID : 'YOUR_COGNITO_USER_POOL_ID',
+    APP_CLIENT_ID : 'YOUR_COGNITO_APP_CLIENT_ID',
+  }
+};
